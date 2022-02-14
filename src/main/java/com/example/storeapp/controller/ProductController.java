e4f153ce40a0f6c7a5c6659c4cdd116e33544690
LeeKyuyoung <pocky1111@naver.com>
2022-02-14 17:13:45 +0900

    search product done

--- src/main/java/com/example/storeapp/controller/ProductController.java
+++ src/main/java/com/example/storeapp/controller/ProductController.java
@@ -6,11 +6,16 @@ package com.example.storeapp.controller;
 import com.example.storeapp.entity.Product;
 import com.example.storeapp.repository.ProductRepository;
 import com.example.storeapp.service.ProductService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -47,21 +52,35 @@ public class ProductController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+
     @RequestMapping(path = "/get/{id}", method = RequestMethod.GET)
     public ResponseEntity<Product> retrieveProduct(@PathVariable Long id) {
         Optional<Product> product = productService.getProductById(id);
         return new ResponseEntity<>(product.get(), HttpStatus.OK);
     }
-    @RequestMapping(path = "/search}", method = RequestMethod.POST)
-    public ResponseEntity<?> searchProducts(@PathVariable String words) {
+
+
+
+    @RequestMapping(path = "/search", method = RequestMethod.POST)
+    public ResponseEntity<?> searchProducts(@RequestBody @Valid StringDto request) {
 
         // Getting all products in application...
         List<Product> list = new ArrayList<>();
-        Iterable<Product> iterable = productService.searchProducts(words);
+        Iterable<Product> iterable = productService.searchProducts(request.getStr());
         for (Product p : iterable) {
             list.add(p);
         }
 
         return ResponseEntity.ok(list);
     }
+
+
+    @Getter
+    @Setter
+    static class StringDto {
+        @NotNull(message = "string is required")
+        @Size(message = "string must be equal to or lower than 300", min = 1, max = 300)
+        private String str;
+    }
 }
+
